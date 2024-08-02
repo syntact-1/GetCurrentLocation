2028dabb1eaf631d148eddbe5b8a337f8cf01a29
Youssef Zidane <151948962+syntact-1@users.noreply.github.com>
2024-08-02 15:43:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let locationButton = document.getElementById("get-location");
+let locationDiv = document.getElementById("location-details");
+
+locationButton.addEventListener("click", () => {
+  //Geolocation APU is used to get geographical position of a user and is available inside the navigator object
+  if (navigator.geolocation) {
+    //returns position(latitude and longitude) or error
+    navigator.geolocation.getCurrentPosition(showLocation, checkError);
+  } else {
+    //For old browser i.e IE
+    locationDiv.innerText = "The browser does not support geolocation";
+  }
+});
+
+//Error Checks
+const checkError = (error) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      locationDiv.innerText = "Please allow access to location";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      //usually fired for firefox
+      locationDiv.innerText = "Location Information unavailable";
+      break;
+    case error.TIMEOUT:
+      locationDiv.innerText = "The request to get user location timed out";
+  }
+};
+
+const showLocation = async (position) => {
+  //We user the NOminatim API for getting actual addres from latitude and longitude
+  let response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+  );
+  //store response object
+  let data = await response.json();
+  console.log(data.address)
+  locationDiv.innerText = `${data.address.city}, ${data.address.country}  \n postcode : ${data.address.postcode} \n village : ${data.address.village}  \n state : ${data.address.state} \n full data in console `;
+};
